c327d4d11cf198f9e0304b1e15addfc20ffaecfa
ggzor <30713864+ggzor@users.noreply.github.com>
2018-08-10 05:54:32 -0500

    Agregando estructuras

--- Programa.c
+++ Programa.c
@@ -1,5 +1,65 @@
 #include <stdio.h>
 
-int main(int argc, char **argv) {
-    return 0;
+typedef struct
+{
+   NodoMesa *mesas;
+   NodoReservacion *reservacion;
+} Restaurante;
+
+typedef struct
+{
+   Mesa *mesa;
+   NodoMesa *siguiente;
+} NodoMesa;
+
+typedef struct
+{
+   Reservacion *reservacion;
+   NodoReservacion *siguiente;
+} NodoReservacion;
+
+typedef struct
+{
+   int numero;
+   int maximoPersonas;
+   NodoReservacion *reservaciones;
+} Mesa;
+
+typedef struct
+{
+   Intervalo intervalo;
+   Cliente cliente;
+} Reservacion;
+
+typedef struct
+{
+   Fecha fecha;
+   Hora inicio;
+   Hora fin;
+} Intervalo;
+
+typedef struct
+{
+   char nombre[50];
+   char direccion[100];
+   long long telefono;
+} Cliente;
+
+typedef struct
+{
+   int dia;
+   int mes;
+   int año;
+} Fecha;
+
+typedef struct
+{
+   int segundo;
+   int minuto;
+   int hora;
+} Hora;
+
+int main(int argc, char **argv)
+{
+   return 0;
 }
