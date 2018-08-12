51c41152a2741ab8613cfcda5f86a19c5d32ff80
ggzor <30713864+ggzor@users.noreply.github.com>
2018-08-12 15:29:45 -0500

    Corrigiendo definicion de estructuras

--- Estructuras.h
+++ Estructuras.h
@@ -1,59 +1,59 @@
 #ifndef _ESTRUCTURAS_H_
 #define _ESTRUCTURAS_H_
 
-typedef struct Cliente
+typedef struct
 {
    char nombre[50];
    char direccion[100];
    long long telefono;
 } Cliente;
 
-typedef struct Fecha
+typedef struct
 {
    int anio;
    int mes;
    int dia;
 } Fecha;
 
-typedef struct Hora
+typedef struct
 {
    int hora;
    int minuto;
 } Hora;
 
-typedef struct Intervalo
+typedef struct
 {
    Fecha fecha;
    Hora inicio;
    Hora fin;
 } Intervalo;
 
-typedef struct Reservacion
+typedef struct
 {
    Cliente cliente;
    Intervalo intervalo;
 } Reservacion;
 
-typedef struct NodoReservacion
+typedef struct
 {
    Reservacion *reservacion;
    struct NodoReservacion *siguiente;
 } NodoReservacion;
 
-typedef struct Mesa
+typedef struct
 {
    int numero;
    int capacidad;
    NodoReservacion *reservaciones;
 } Mesa;
 
-typedef struct NodoMesa
+typedef struct
 {
    Mesa *mesa;
    struct NodoMesa *siguiente;
 } NodoMesa;
 
-typedef struct Restaurante
+typedef struct
 {
    int mesaCapacidadMaxima;
    NodoMesa *mesas;
