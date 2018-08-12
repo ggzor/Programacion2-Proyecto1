6be288cf5d1df6f2d4be1b94d302ea68de207d87
ggzor <30713864+ggzor@users.noreply.github.com>
2018-08-12 16:37:16 -0500

    Agregando programa de prueba

--- Programa.c
+++ Programa.c
@@ -1,42 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 #include "./Estructuras.h"
-#include "./Almacenamiento.h"
-
-void intentarReservar(Restaurante *restaurante){
-    int numeroPersonas;
-    verificarMesaConCapacidad(restaurante, numeroPersonas);
-    Intervalo horario = leerHorario();
-    Mesa *mesa = obtenerMesaDisponibleEnHorario(restaurante, horario);
-    Cliente cliente = leerCliente();
-    Reservacion reservacion = { horario, cliente };
-    realizarReservacion(mesa, reservacion);
-    imprimirRecibo(mesa, reservacion);
-}
+#include "./DatosPrueba.h"
+#include "./Impresion.h"
 
 int main(int argc, char **argv)
 {
-    int i;
-    Restaurante *restaurante;
-    restaurante = cargarInformacion();
-
-    hacerReservacion(restaurante, solicitarReservacion());
-
-    printf(
-        "1.- Hacer reservación\n"
-        "2.- Cancelar reservación\n"
-        "3.- Buscar reservación\n"
-        "4.- Reservaciones canceladas\n"
-        "5.- Ver horarios de mesa\n"
-        "6.- Salir\n");
-
-    guardarInformacion(restaurante);
+    Restaurante *restaurante = obtenerDatos();
+    imprimirRestaurante(restaurante);
+    
     return 0;
 }
-
-
-Cliente leerCliente();
-Hora leerHora();
-Fecha leerFecha();
-Intervalo leerIntervalo();
