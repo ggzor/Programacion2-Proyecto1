d6f6d086c675d200d7f6f3abcd34f0fb0a7020be
ggzor <30713864+ggzor@users.noreply.github.com>
2018-08-19 07:56:41 -0500

    Cambiando comportamiento de Ver Horarios

--- Operaciones/VerHorarios.c
+++ Operaciones/VerHorarios.c
@@ -1,7 +1,11 @@
+#include <stdio.h>
 #include "../Operaciones.h"
 
 void verHorarios(Restaurante *restaurante)
 {
   puts("Los horarios por cada mesa son los siguentes:\n");
   imprimirHorariosRestauranteFormatoCorto(restaurante);
+
+  puts("Presione enter para volver al menú...");
+  getchar();
 }
