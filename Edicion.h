2d7e34cbc95ed3719197174a9cc9a73676207ab4
ggzor <30713864+ggzor@users.noreply.github.com>
2018-08-21 23:24:38 -0500

    Removiendo edición de horario

--- Edicion.h
+++ Edicion.h
@@ -1,15 +1,8 @@
 #ifndef _EDICION_H_
 #define _EDICION_H_
 
-#include <stdio.h>
 #include "Estructuras.h"
-#include "Tiempo/IntervaloHoras.h"
-#include "Tiempo/IntervaloFechas.h"
-#include "Tiempo/Horario.h"
-#include "Tiempo/FechaHora.h"
-#include "Lectura.h"
 
 void editarCliente(Cliente *cliente);
-void editarHorario(FechaHora *despuesDe, IntervaloHoras *limitesHora, Fecha *fechaMaxima, Horario *horario);
 
 #endif
