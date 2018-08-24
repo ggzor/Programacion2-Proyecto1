0b383219a076b05c364e0ac207f9ebba4e8fbf0e
ggzor <30713864+ggzor@users.noreply.github.com>
2018-08-23 21:00:11 -0500

    Agregando operaciones extra de búsqueda y confirmación de reservaciones.

--- OperacionesUsuario/BuscarReservacion.c
+++ OperacionesUsuario/BuscarReservacion.c
@@ -1,5 +1,45 @@
 #include "Menu.h"
+#include "../Interfaz.h"
+#include <math.h>
+#include "../Configuracion.h"
+#include "../Impresion.h"
+#include "../Reservaciones.h"
 
 void buscarReservacion(Restaurante *restaurante)
 {
+  int clave, claveMaxima = pow(16, DigitosClaveReservacion);
+  Reservacion *reservacion;
+
+  imprimirAdvertencia(printf("La clave debe estar entre %04X y %04X\n", 0, claveMaxima - 1));
+  printf("Introduzca su clave de reservación: ");
+  scanf("%X%*c", &clave);
+
+  if (0 <= clave && clave < claveMaxima)
+  {
+    reservacion = buscarReservacionPorClave(restaurante, clave);
+
+    if (reservacion == NULL)
+    {
+      reservacion = buscarReservacionCanceladaPorClave(restaurante, clave);
+
+      if (reservacion == NULL)
+      {
+        imprimirError(printf("La reservación no existe."));
+      }
+      else
+      {
+        imprimirError(puts("La reservación está cancelada."));
+        imprimirReservacion(reservacion);
+      }
+    }
+    else
+    {
+      imprimirReservacion(reservacion);
+    }
+  }
+  else
+  {
+    imprimirError(puts("La clave está fuera de los límites."));
+  }
+  pausar();
 }
--- OperacionesUsuario/NotificarLlegada.c
+++ OperacionesUsuario/NotificarLlegada.c
@@ -1,6 +1,45 @@
 #include "Menu.h"
+#include "../Configuracion.h"
+#include "../Reservaciones.h"
+#include "../Interfaz.h"
+#include "math.h"
 
-void notificarLlegada(Restaurante *restaurane)
+void notificarLlegada(Restaurante *restaurante)
 {
-  
+  int clave, claveMaxima = pow(16, DigitosClaveReservacion);
+  Reservacion *reservacion;
+
+  imprimirAdvertencia(printf("La clave debe estar entre %04X y %04X\n", 0, claveMaxima - 1));
+  printf("Introduzca su clave de reservación: ");
+  scanf("%X%*c", &clave);
+
+  if (0 <= clave && clave < claveMaxima)
+  {
+    reservacion = buscarReservacionPorClave(restaurante, clave);
+
+    if (reservacion == NULL)
+    {
+      reservacion = buscarReservacionCanceladaPorClave(restaurante, clave);
+
+      if (reservacion == NULL)
+      {
+        imprimirError(printf("La reservación no existe."));
+      }
+      else
+      {
+        imprimirError(puts("La reservación ya fue cancelada porque excedió el tiempo de tolerancia."));
+        imprimirReservacion(reservacion);
+      }
+    }
+    else
+    {
+      reservacion->confirmada = 1;
+      enVerde(puts("Reservación confirmada."));
+    }
+  }
+  else
+  {
+    imprimirError(puts("La clave está fuera de los límites."));
+  }
+  pausar();
 }
