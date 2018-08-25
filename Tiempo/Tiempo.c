8e1c055e5f36a11a32774a66ca620d7709169b79
ggzor <30713864+ggzor@users.noreply.github.com>
2018-08-24 22:46:54 -0500

    Actualizando código de tiempo

--- Tiempo/Tiempo.c
+++ Tiempo/Tiempo.c
@@ -1,40 +1,8 @@
-#include <time.h>
 #include "Tiempo.h"
-
-FechaHora ahoraDeComputadora();
-int cambiado = 0;
-FechaHora ahora;
-
-FechaHora obtenerAhora()
-{
-  if (cambiado)
-  {
-    return ahora;
-  }
-  else
-  {
-    return ahoraDeComputadora();
-  }
-}
-
-void establecerAhora(FechaHora nuevoAhora)
-{
-  ahora = nuevoAhora;
-  cambiado = 1;
-}
-
-void reestablecerAhora()
-{
-  cambiado = 0;
-}
-
-Fecha obtenerHoy()
-{
-  return obtenerAhora().fecha;
-}
+#include <time.h>
 
 // Basado en: https://stackoverflow.com/questions/1442116/how-to-get-the-date-and-time-values-in-a-c-program
-FechaHora ahoraDeComputadora()
+FechaHora obtenerAhora()
 {
   time_t tiempo = time(NULL);
   struct tm valores = *localtime(&tiempo);
@@ -45,6 +13,11 @@ FechaHora ahoraDeComputadora()
        .dia = valores.tm_mday},
       {.hora = valores.tm_hour,
        .minuto = valores.tm_min}};
-       
+
   return ahora;
+}
+
+Fecha obtenerHoy()
+{
+  return obtenerAhora().fecha;
 }
--- Tiempo/Tiempo.h
+++ Tiempo/Tiempo.h
@@ -1,13 +1,10 @@
 #ifndef _TIEMPO_H_
 #define _TIEMPO_H_
 
-#include "Fecha.h"
-#include "FechaHora.h"
+#include "Primitivas/Fecha.h"
+#include "Primitivas/FechaHora.h"
 
 FechaHora obtenerAhora();
-void establecerAhora(FechaHora nuevoAhora);
-void reestablecerAhora();
-
 Fecha obtenerHoy();
 
 #endif
