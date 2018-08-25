29fc21e4993faee74830b3000bd8d4ba8990a98b
ggzor <30713864+ggzor@users.noreply.github.com>
2018-08-25 13:02:08 -0500

    Actualizando valores de prueba

--- Datos/Pruebas/ValoresPrueba.c
+++ Datos/Pruebas/ValoresPrueba.c
@@ -9,36 +9,30 @@ Restaurante *obtenerDatosPrueba()
 
 	Mesa *mesa1 = crearMesa(1, 4);
 
-	Horario intervalo1_1 =  { { 2018, 8, 25 }, { { 9, 10 }, { 10, 40 } }	};
+	Horario intervalo1_1 =  { { 2018, 8, 29 }, { { 9, 30 }, { 10, 0 } }	};
 	agregarReservacion(mesa1, crearReservacion(4, cliente, intervalo1_1));
 
-	Horario intervalo1_2 =  { { 2018, 8, 22 }, { { 9, 30 }, { 10, 0 } }	};
+	Horario intervalo1_2 =  { { 2018, 8, 30 }, { { 15, 0 }, { 16, 0 } }	};
 	agregarReservacion(mesa1, crearReservacion(4, cliente, intervalo1_2));
 
-	Horario intervalo1_3 =  { { 2018, 8, 23 }, { { 10, 0 }, { 12, 0 } }	};
+	Horario intervalo1_3 =  { { 2018, 8, 30 }, { { 10, 0 }, { 12, 0 } }	};
 	agregarReservacion(mesa1, crearReservacion(4, cliente, intervalo1_3));
 
-	Horario intervalo1_4 =  { { 2018, 8, 23 }, { { 15, 0 }, { 16, 0 } }	};
+	Horario intervalo1_4 =  { { 2018, 8, 30 }, { { 13, 0 }, { 14, 0 } }	};
 	agregarReservacion(mesa1, crearReservacion(4, cliente, intervalo1_4));
 
-	Horario intervalo1_5 =  { { 2018, 8, 23 }, { { 13, 0 }, { 14, 0 } }	};
+	Horario intervalo1_5 =  { { 2018, 8, 30 }, { { 9, 0 }, { 10, 0 } }	};
 	agregarReservacion(mesa1, crearReservacion(4, cliente, intervalo1_5));
-
-	Horario intervalo1_6 =  { { 2018, 8, 23 }, { { 9, 0 }, { 10, 0 } }	};
-	agregarReservacion(mesa1, crearReservacion(4, cliente, intervalo1_6));
-
-	Horario intervalo1_7 =  { { 2018, 8, 24 }, { { 9, 0 }, { 10, 0 } }	};
-	agregarReservacion(mesa1, crearReservacion(4, cliente, intervalo1_7));
 
 	agregarMesa(restaurante, mesa1);
 
 
 	Mesa *mesa2 = crearMesa(2, 6);
 
-	Horario intervalo2_1 =  { { 2018, 8, 22 }, { { 9, 0 }, { 9, 30 } }	};
+	Horario intervalo2_1 =  { { 2018, 8, 30 }, { { 9, 0 }, { 9, 30 } }	};
 	agregarReservacion(mesa2, crearReservacion(6, cliente, intervalo2_1));
 
-	Horario intervalo2_2 =  { { 2018, 8, 22 }, { { 16, 0 }, { 18, 30 } }	};
+	Horario intervalo2_2 =  { { 2018, 8, 30 }, { { 16, 0 }, { 18, 30 } }	};
 	agregarReservacion(mesa2, crearReservacion(6, cliente, intervalo2_2));
 
 	agregarMesa(restaurante, mesa2);
@@ -46,7 +40,7 @@ Restaurante *obtenerDatosPrueba()
 
 	Mesa *mesa3 = crearMesa(3, 10);
 
-	Horario intervalo3_1 =  { { 2018, 8, 23 }, { { 13, 0 }, { 15, 0 } }	};
+	Horario intervalo3_1 =  { { 2018, 8, 30 }, { { 13, 0 }, { 15, 0 } }	};
 	agregarReservacion(mesa3, crearReservacion(10, cliente, intervalo3_1));
 
 	agregarMesa(restaurante, mesa3);
