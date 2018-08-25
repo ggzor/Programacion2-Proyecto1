27221aa6d5227774c5c9e9793106afec902ad87b
ggzor <30713864+ggzor@users.noreply.github.com>
2018-08-25 14:45:33 -0500

    Agregando scripts para depuración

--- Scripts/generarCodigoCompilacion.py
+++ Scripts/generarCodigoCompilacion.py
@@ -19,7 +19,12 @@ directorio = Path('.')
 archivos = ['{}'.format(p) for p in obtenerArchivosC(directorio)]
 todosArchivos = ' '.join(archivos)
 comando = 'gcc {} -o Programa -lm'
+comandoDepuracion = comando + ' -g'
 
 with open('./Scripts/invocar_compilador', 'w') as destino:
     destino.write('#!/bin/bash\n\n')
     destino.write(comando.format(todosArchivos))
+
+with open('./Scripts/invocar_compilador_depuracion', 'w') as destino:
+    destino.write('#!/bin/bash\n\n')
+    destino.write(comandoDepuracion.format(todosArchivos))
