73ead458dc2f8f975b709ec4c1b3758922855ea6
ggzor <30713864+ggzor@users.noreply.github.com>
2018-08-25 21:06:26 -0500

    Terminando implementacion de manejo de tiempo

--- Operaciones/Pruebas/EstablecerTiempo.c
+++ Operaciones/Pruebas/EstablecerTiempo.c
@@ -2,27 +2,56 @@
 #include "../../Datos/Lectura.h"
 #include "../../Interfaz/Interfaz.h"
 #include "../../Tiempo/Impresion.h"
+#include "../../Tiempo/Tiempo.h"
 #include "../../Tiempo/ManejoTiempo.h"
 #include <stdio.h>
 
 void establecerTiempo(Restaurante *restaurante)
 {
+  int cambiarFecha, cambiarHora, modificado = 0;
   IntervaloFechas limitesFecha = {
-      {2000, 1, 1},
-      {2100, 1, 1}};
+      {2018, 8, 1},
+      {2018, 12, 31}};
   IntervaloHoras todoElDia = {
       {0, 0}, {23, 59}};
-  FechaHora nueva;
+  FechaHora actual = obtenerAhora();
+  FechaHora nueva = actual;
 
-  puts("Seleccione los valores para el tiempo actual: ");
-  nueva.fecha = leerFecha(&limitesFecha);
-  nueva.hora = leerHora(&todoElDia);
+  puts("Establecer tiempo:\n");
 
-  printf("Estableciendo tiempo: ");
-  imprimirFechaHora(&nueva);
+  imprimirCadena("El tiempo actual es: ");
+  imprimirFechaHora(&actual);
   puts("");
 
-  establecerAhora(nueva);
+  leerSiNo("¿Desea modificar la fecha [s/n]? ", &cambiarFecha);
 
+  if (cambiarFecha)
+  {
+    nueva.fecha = leerFecha(&limitesFecha);
+    modificado = 1;
+  }
+
+  leerSiNo("¿Desea modificar la hora [s/n]? ", &cambiarHora);
+
+  if (cambiarHora)
+  {
+    nueva.hora = leerHora(&todoElDia);
+    modificado = 1;
+  }
+
+  if (modificado)
+  {
+    printf("Estableciendo tiempo: ");
+    imprimirFechaHora(&nueva);
+    puts("");
+
+    establecerAhora(nueva);
+  }
+  else
+  {
+    imprimirAdvertencia(puts("No se modificó el tiempo actual."));
+  }
+
+  puts("");
   pausar();
 }
--- Operaciones/Pruebas/RestablecerTiempo.c
+++ Operaciones/Pruebas/RestablecerTiempo.c
@@ -7,13 +7,25 @@
 
 void restablecerTiempo(Restaurante *restaurante)
 {
+  int confirmado;
   FechaHora ahora;
 
-  puts("Reestableciendo a tiempo actual.");
-  reestablecerAhora();
-  ahora = obtenerAhora();
-  printf("Tiempo actual: ");
-  imprimirFechaHora(&ahora);
+  puts("Reestableciendo a tiempo actual:\n");
+  leerSiNo("¿Está seguro que desea restablecer el tiempo actual [s/n]? ", &confirmado);
 
+  if (confirmado)
+  {
+    reestablecerAhora();
+    ahora = obtenerAhora();
+    printf("Tiempo actual: ");
+    imprimirFechaHora(&ahora);
+    puts("");
+  }
+  else
+  {
+    imprimirAdvertencia(puts("No se ha modificado el tiempo actual."));
+  }
+
+  puts("");
   pausar();
 }
