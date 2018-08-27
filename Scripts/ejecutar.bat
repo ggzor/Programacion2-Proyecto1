0146f1edb9ee83742f8ce027b5299e9ea230397b
ggzor <30713864+ggzor@users.noreply.github.com>
2018-08-27 00:38:07 -0500

    Actualizando script con instrucciones

--- Scripts/ejecutar.bat
+++ Scripts/ejecutar.bat
@@ -3,6 +3,15 @@ rem Para restaurar si hay error
 set directorio=%CD%
 
 chcp 65001
+IF NOT EXIST .configurado (
+  echo [33mIMPORTANTE: Cambia el tipo de fuente de la consola para que se muestren los caracteres correctamente[0m
+  echo https://www.downloadsource.es/como-personalizar-la-fuente-y-color-de-simbolo-del-sistema-letras-y-fondo-del-cmd/n/9269/
+  echo Se recomienda usar la fuente 'DejaVu Sans Mono'
+  echo Los caracteres siguientes deberían mostrarse correctamente: ↻ ✔ ⚑ ◴ ◴ ✘
+  echo. 2>.configurado
+  pause
+)
+
 Scripts\compilar^
  && echo [1mComenzando ejecución...[0m^
  && Programa.exe
